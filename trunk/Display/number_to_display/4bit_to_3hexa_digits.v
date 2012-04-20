a5af8b7609b35ac8dc79bc30f8fe9c4aae38e3f4
jpdarago@gmail.com <jpdarago@gmail.com@964a6f78-6e68-071e-eae8-19a2fa39957a>
2012-04-20 15:49:09 +0000

    arreglado bug en 4bit_to_3hexa_digits.v

--- trunk/Display/number_to_display/4bit_to_3hexa_digits.v
+++ trunk/Display/number_to_display/4bit_to_3hexa_digits.v
@@ -35,7 +35,7 @@ module bit_to_3hexa_digits(
 			end
 			3'b110:
 			begin
-				h1 = {3'b001, entrada[3]};
+				h1 = {3'b001, entrada[0]};
 				h2 = 4'b0001;
 			end
 			3'b000, 3'b001, 3'b010, 3'b011, 3'b100: //3'b0??:
