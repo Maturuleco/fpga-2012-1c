fc65d1080bcdf46543f7b768491528a8ff7b4b7c
elmaildematiaz <elmaildematiaz@964a6f78-6e68-071e-eae8-19a2fa39957a>
2012-05-11 16:09:50 +0000

    cola fifo andando

--- trunk/Cola_fifo/cola_fifo.v
+++ trunk/Cola_fifo/cola_fifo.v
@@ -19,36 +19,44 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module cola_fifo
-#( parameter B=8, // number of bits in a word
-	W=4 // number of address bits
+#( parameter W=8, // number of bits in a word
+	A=4 // number of address bits
 )
 (
 	input wire clk, reset,
 	input rd,
 	input wr,
-	input [B-1:0] in,
-	output [B-1:0] data,
+	input [W-1:0] in,
+	output reg [W-1:0] data,
 	output full,
 	output error,
-	output empty
+	output empty,
+	output reg [A-1:0] w_ptr_reg,
+	output reg [A-1:0] r_ptr_reg
 	);
 	
 	//signal declaration
-	reg [W-1:0] w_ptr_reg, w_ptr_next;
-	reg [W-1:0] r_ptr_reg, r_ptr_next;
+	reg [A-1:0] w_ptr_next;//, w_ptr_reg;
+	reg [A-1:0] r_ptr_next;//, r_ptr_reg;
+	
+	wire [A-1:0] w_ptr_succ, r_ptr_succ;
+	assign w_ptr_succ = w_ptr_reg +1;
+	assign r_ptr_succ = r_ptr_reg +1;
+	
 	reg full_reg, empty_reg, full_next, empty_next;
 	wire wr_en;
 	wire rd_en;
 
 	// write enabled only when FIFO is not full
-	assign wr_en = wr & ~full_reg;
-	assign rd_en = rd & ~empty_reg;
-	assign error = (wr && full_reg) || (rd && empty_reg);
+	assign wr_en = wr & ~full;
+	assign rd_en = rd & ~empty;
+	assign error = (wr && full) || (rd && empty);
 	
 	assign full = full_reg;
 	assign empty = empty_reg;
 	
-	banco_registros #(.W(B), .A(W)) memoria ( .clk(clk), .data_in(in), .data_out2(data), .address_w(w_ptr_reg), .address_r(r_ptr_reg), .wr_en(wr_en) );
+	wire [W-1:0] salida;
+	banco_registros #(.W(W), .A(A)) memoria ( .clk(clk), .data_in(in), .data_out2(salida), .address_w(w_ptr_reg), .address_r(r_ptr_reg), .wr_en(wr_en) );
 	//test_mem3 memoria ( .clk(clk), .data_in(in), .data_out(data), .data(data2), .address_w(w_ptr_reg), .address_r(r_ptr_reg), .wr_en(wr_en) );
 		
 	// Memoria FSM
@@ -69,50 +77,69 @@ module cola_fifo
 			empty_reg <= empty_next;
 		end
 	end
-	
+
+	// Logica de Salida
+	always@*
+	begin
+		if ( empty )
+			data = 0;
+		else
+			data = salida;
+	end
+
 	localparam [1:0]
 		pop = 2'b01,
 		push = 2'b10,
 		idle = 2'b00,
 		pop_push = 2'b11;
+
+	// Logica de Punteros
+	always@*
+	begin
+		case ({wr_en, rd_en})
+			pop:
+			begin
+				r_ptr_next = (r_ptr_succ);
+			end
+			push:
+			begin
+				w_ptr_next = (w_ptr_succ);
+			end
+			idle:
+			begin
+				r_ptr_next = r_ptr_reg;
+				w_ptr_next = w_ptr_reg;
+			end
+			pop_push:
+			begin
+				r_ptr_next = (r_ptr_succ);
+				w_ptr_next = (w_ptr_succ);
+			end
+			default:
+			begin
+				r_ptr_next = 0;
+				w_ptr_next = 0;
+			end
+		endcase
+	end
 	
-	// Logica
+	// Logica de Señales (E/F)
 	always@ *
 	begin
 		full_next = full_reg;
 		empty_next = empty_reg;
-		r_ptr_next = r_ptr_reg;
-		w_ptr_next = w_ptr_reg;
 		case ({wr_en, rd_en})
 			pop:
 			begin
-				r_ptr_next = r_ptr_reg + 1;
 				full_next = 1'b0;
-				if ((r_ptr_reg + 1) == w_ptr_reg)
+				if (r_ptr_succ == w_ptr_reg)
 					empty_next = 1'b1;
 			end
 			push:
 			begin
-				w_ptr_next = w_ptr_reg +1;
 				empty_next = 1'b0;
-				if ((w_ptr_reg +1) == r_ptr_reg)
+				if (w_ptr_succ == r_ptr_reg)
 					full_next = 1'b1;
-			end
-			//idle: no op
-			pop_push:
-			begin
-				r_ptr_next = r_ptr_reg+1;
-				w_ptr_next = w_ptr_reg+1;
-			end
-			idle:
-			begin
-			end
-			default:
-			begin
-				w_ptr_next = 0;
-				r_ptr_next = 0;
-				full_next = 1'b0;
-				empty_next = 1'b1;
 			end
 		endcase
 	end
--- trunk/Cola_fifo/test_fifo.v
+++ trunk/Cola_fifo/test_fifo.v
@@ -6,38 +6,41 @@ module test_fifo
 input wire clk, reset,
 input wire wr,
 input wire rd,
-input wire [2:0] sw,
-output wire [7:0] led
+input wire [W-1:0] sw,
+output wire [7:0] led,
+	output wire [A:0] w_ptr_reg,
+	output wire [A:0] r_ptr_reg,
+	output wire pop, push
 );
 
-	// debounce
-//	wire db_wr, db_rd;
-//	db_fsm btn_rd (.clk(clk), .reset(reset), .sw(rd), .db(db_rd));
-//	db_fsm btn_wr (.clk(clk), .reset(reset), .sw(wr), .db(db_wr));
-	
-	el full está mal!!!
+	localparam A=2, W=3;
 
-	wire rd_fifo, wr_fifo;
-	button_regulator br_rd ( .clk(clk), .bt_in(rd) , .bt_out(rd_fifo) );
-	button_regulator br_wr ( .clk(clk), .bt_in(wr) , .bt_out(wr_fifo) );
+	// debounce
+	wire db_push, db_pop;
+	db_fsm #(.N(2)) btn_rd (.clk(clk), .reset(reset), .sw(rd), .db(db_pop));
+	db_fsm #(.N(2)) btn_wr (.clk(clk), .reset(reset), .sw(wr), .db(db_push));
+	
+	//wire pop, push;
+	button_regulator br_rd ( .clk(clk), .reset(reset), .bt_in(db_pop) , .bt_out(pop) );
+	button_regulator br_wr ( .clk(clk), .reset(reset), .bt_in(db_push) , .bt_out(push) );
 
 	// instantiate a 2^2-by-3 fifo
-	cola_fifo #(.B(3), .W(2)) fifo_unit
+	cola_fifo #(.W(W), .A(A)) fifo_unit
 	(
 		.clk(clk), .reset(reset),
-		//.rd(db_btn[0]),
-		//.wr(db_btn[1]),
-		.rd(rd_fifo),
-		.wr(wr_fifo),
+		.rd(pop),
+		.wr(push),
 		.in(sw),
-		.data(led[2:0]),
+		.data(led[W-1:0]),
 		.full(led[7]),
 		.empty(led[6]),
-		.error(led[5])
+		.error(led[5]),
+		.r_ptr_reg(r_ptr_reg),
+		.w_ptr_reg(w_ptr_reg)
 	);
 
 	// disable unused leds
 
-	assign led[4:3] = 2'b00;
+	assign led[4:W] = 2'b00;
 
 endmodule
