21e5351b46bbca92486b98646b3b5290c7681f92
elmaildematiaz <elmaildematiaz@964a6f78-6e68-071e-eae8-19a2fa39957a>
2012-05-11 16:11:25 +0000

    Arreglos en el debounce y refactor de boton muevo sources de memorias a memoria

--- button/db_fsm.v
+++ button/db_fsm.v
@@ -18,11 +18,15 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module db_fsm(
+module db_fsm
+#( parameter N=20 )// number of counter bits (2^N * 20ns = 10ms tick)
+(
     input clk,
     input reset,
     input sw,
-    output reg db
+    output reg db,
+	 output reg [2:0] state_next,
+	 output m_tick
     );
 
  // symbolic state declaration
@@ -36,32 +40,34 @@ module db_fsm(
                wait0_2 = 3'b110,
                wait0_3 = 3'b111;
 
-   // number of counter bits (2^N * 20ns = 10ms tick)
-   localparam N =19;
+
 
    // signal declaration
    reg [N-1:0] q_reg;
    wire [N-1:0] q_next;
-   wire m_tick;
-   reg [2:0] state_reg, state_next;
+   //wire m_tick;
+   reg [2:0] state_reg;//, state_next;
 
    // body
 
    //=============================================
    // counter to generate 10 ms tick
    //=============================================
-   always @(posedge clk)
-      q_reg <= q_next;
+   always @(posedge clk, posedge reset)
+		if (reset)
+			q_reg <= 0;
+		else
+			q_reg <= q_next;
    // next-state logic
    assign q_next = q_reg + 1;
    // output tick
-   assign m_tick = (q_reg==0) ? 1'b1 : 1'b0;
+   assign m_tick = (q_reg==0);
 
    //=============================================
    // debouncing FSM
    //=============================================
    // state register
-    always @(posedge clk, posedge reset)
+    always @(posedge m_tick, posedge reset)
        if (reset)
           state_reg <= zero;
        else
@@ -80,20 +86,17 @@ module db_fsm(
             if (~sw)
                state_next = zero;
             else
-               if (m_tick)
-                  state_next = wait1_2;
+               state_next = wait1_2;
          wait1_2:
             if (~sw)
                state_next = zero;
             else
-               if (m_tick)
-                  state_next = wait1_3;
+               state_next = wait1_3;
          wait1_3:
             if (~sw)
                state_next = zero;
             else
-               if (m_tick)
-                  state_next = one;
+               state_next = one;
          one:
             begin
               db = 1'b1;
@@ -106,8 +109,7 @@ module db_fsm(
                if (sw)
                   state_next = one;
                else
-                 if (m_tick)
-                    state_next = wait0_2;
+                  state_next = wait0_2;
             end
          wait0_2:
             begin
@@ -115,8 +117,7 @@ module db_fsm(
                if (sw)
                   state_next = one;
                else
-                 if (m_tick)
-                    state_next = wait0_3;
+                  state_next = wait0_3;
             end
          wait0_3:
             begin
@@ -124,10 +125,10 @@ module db_fsm(
                if (sw)
                   state_next = one;
                else
-                 if (m_tick)
-                    state_next = zero;
+                  state_next = zero;
             end
-         default: state_next = zero;
+         default:
+				state_next = zero;
       endcase
    end
 
