206b3fda92f6feff2e3c13b2a4e556de6e3b5224
elmaildematiaz <elmaildematiaz@964a6f78-6e68-071e-eae8-19a2fa39957a>
2012-06-29 15:52:47 +0000

    obstacles

--- Trabajo Final/src/RoadFighter/main.v
+++ Trabajo Final/src/RoadFighter/main.v
@@ -20,6 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 module main(
     input wire clk, reset,
+//	 input wire start,
 	 input wire left, right,
     output wire hsync, vsync,
     output wire [2:0] rgb
@@ -31,7 +32,11 @@ module main(
    wire video_on;
 	
 	wire [9:0] pixel_x,pixel_y;
-
+	wire upsig;
+	
+	wire [18:0] counter;
+	upclk updatesignal (.clk(clk), .thresh0(upsig), .q(counter));
+	
    // instantiate vga sync circuit
    vga_sync vsync_unit
       (.clk(clk), .reset(reset), .hsync(hsync), .vsync(vsync),
@@ -43,27 +48,91 @@ module main(
          rgb_reg <= 0;
       else
          rgb_reg <= rgb_next;
+	
+	// -------------------------------------------------------------------------
 
 	wire [7:0] player_car_x;
 	wire [9:0] player_car_y;
 	wire on_player_car;
 	wire [2:0] rgb_car_player;
 
-	player p1 (.clk(clk), .reset(reset), .left(left), .right(right), 
+	player p1 (.clk(upsig), .reset(reset), .left(left), .right(right), 
 						.car_x(player_car_x), .car_y(player_car_y) );
 
 	graphic_car_controller p1_gcontroller (
 					.car_position_x(player_car_x), .car_position_y(player_car_y), 
 					.pixel_x(pixel_x), .pixel_y(pixel_y), .on(on_player_car), .rgb(rgb_car_player) );
+					
+	// -------------------------------------------------------------------------
+	
+	wire obs_0_on, obs_1_on, obs_2_on, obs_3_on, obs_4_on, obs_5_on;
+	wire [7:0] obs_0_x, obs_1_x, obs_2_x, obs_3_x, obs_4_x, obs_5_x, obs_6_x;
+	wire [9:0] obs_0_y, obs_1_y, obs_2_y, obs_3_y, obs_4_y, obs_5_y, obs_6_y;
+	
+	obstacle_manager obs_manager ( .clk(clk), .on(!reset), .q(counter), .upsig(upsig),
+		.obstacle_on({obs_0_on, obs_1_on, obs_2_on, obs_3_on, obs_4_on, obs_5_on}),
+		.obstacle_x({obs_0_x, obs_1_x, obs_2_x, obs_3_x, obs_4_x, obs_5_x, obs_6_x}),
+		.obstacle_y({obs_0_y, obs_1_y, obs_2_y, obs_3_y, obs_4_y, obs_5_y, obs_6_y}) );
 
+	wire [2:0] rgb_obs_0, rgb_obs_1, rgb_obs_2, rgb_obs_3, rgb_obs_4, rgb_obs_5;
+	wire obs_0_visual_on, obs_1_visual_on, obs_2_visual_on, obs_3_visual_on, obs_4_visual_on, obs_5_visual_on;
+	
+	graphic_car_controller obs_0_gcont ( .car_position_x(obs_0_x), .car_position_y(obs_0_y),
+					.pixel_x(pixel_x), .pixel_y(pixel_y), .on(obs_0_visual_on), .rgb(rgb_obs_0) );
+	graphic_car_controller obs_1_gcont ( .car_position_x(obs_1_x), .car_position_y(obs_1_y),
+					.pixel_x(pixel_x), .pixel_y(pixel_y), .on(obs_1_visual_on), .rgb(rgb_obs_1) );
+	graphic_car_controller obs_2_gcont ( .car_position_x(obs_2_x), .car_position_y(obs_2_y),
+					.pixel_x(pixel_x), .pixel_y(pixel_y), .on(obs_2_visual_on), .rgb(rgb_obs_2) );
+	graphic_car_controller obs_3_gcont ( .car_position_x(obs_3_x), .car_position_y(obs_3_y),
+					.pixel_x(pixel_x), .pixel_y(pixel_y), .on(obs_3_visual_on), .rgb(rgb_obs_3) );
+	graphic_car_controller obs_4_gcont ( .car_position_x(obs_4_x), .car_position_y(obs_4_y),
+					.pixel_x(pixel_x), .pixel_y(pixel_y), .on(obs_4_visual_on), .rgb(rgb_obs_4) );
+	graphic_car_controller obs_5_gcont ( .car_position_x(obs_5_x), .car_position_y(obs_5_y),
+					.pixel_x(pixel_x), .pixel_y(pixel_y), .on(obs_5_visual_on), .rgb(rgb_obs_5) );
+
+	// -------------------------------------------------------------------------
+	
 	wire [2:0] rgb_bg;
-	background bg (.pixel_x(pixel_x[9:8]), .rgb(rgb_bg));
+	background bg (.pixel_x(pixel_x), .pixel_y(pixel_y), .rgb(rgb_bg), .clk(upsig), .reset(reset));
 
 	graphic_controller gc (
-				.rgb(rgb_next), .on_objs({1'b1,on_player_car}),
-				.r_objs({rgb_bg[0],rgb_car_player[0]}),
-				.g_objs({rgb_bg[1],rgb_car_player[1]}),
-				.b_objs({rgb_bg[2],rgb_car_player[2]})
+				.rgb(rgb_next),
+				.on_objs({
+					on_player_car,
+					obs_0_on & obs_0_visual_on,
+					obs_1_on & obs_1_visual_on,
+					obs_2_on & obs_2_visual_on,
+					obs_3_on & obs_3_visual_on,
+					obs_4_on & obs_4_visual_on,
+					obs_5_on & obs_5_visual_on,
+					1'b1}),
+				.r_objs({
+					rgb_car_player[0],
+					rgb_obs_0[0],
+					rgb_obs_1[0],
+					rgb_obs_2[0],
+					rgb_obs_3[0],
+					rgb_obs_4[0],
+					rgb_obs_5[0],
+					rgb_bg[0]}),
+				.g_objs({
+					rgb_car_player[1],
+					rgb_obs_0[1],
+					rgb_obs_1[1],
+					rgb_obs_2[1],
+					rgb_obs_3[1],
+					rgb_obs_4[1],
+					rgb_obs_5[1],
+					rgb_bg[1]}),
+				.b_objs({
+					rgb_car_player[2],
+					rgb_obs_0[1],
+					rgb_obs_1[1],
+					rgb_obs_2[1],
+					rgb_obs_3[1],
+					rgb_obs_4[1],
+					rgb_obs_5[1],
+					rgb_bg[2]})
 				);
 				
    // output
